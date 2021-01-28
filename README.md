593d4332ce3568c801ef943180cee382cc04c077
Jeremy Peyrat <prstoad@gmail.com>
2021-01-28 11:49:39 +0100

    before dotenv

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ google autoriser et rentrer email
 param projet
 down to config
 copie paste in your project
+
+on configure cloud firestore en mode test
+
+
+library animation react flipmove dans le composant Message forwardRef
+dans le composant Chat nous pouvons utiliser la balise Flipmove
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,15 +1,35 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 import './SidebarChat.css';
+import { setChat} from "./features/chatSlice";
+import db from './firebase';
 
 function SidebarChat({id, chatName}) {
+  const dispatch = useDispatch()
+  const [chatInfo, setChatInfo] = useState([])
+
+  useEffect(() => {
+    db.collection('chats').doc(id).collection('messages').orderBy('timestamp', 'desc')
+    .onSnapshot(snapshot => (
+      setChatInfo(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, [id])
+
   return (
-    <div className="sidebarChat">
-      <Avatar />
+    <div onClick={() => {
+      dispatch(
+        setChat({
+          chatId: id,
+          chatName: chatName
+        })
+      )
+    }} className="sidebarChat">
+      <Avatar src={chatInfo[0]?.photo}/>
       <div className="sidebarChat__info">
         <h3>{chatName}</h3>
-        <p>Last message sent...</p>
-        <small>Timestamp</small>
+        <p>{chatInfo[0]?.message}</p>
+        <small>{new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString()}</small>
       </div>
     </div>
   )
